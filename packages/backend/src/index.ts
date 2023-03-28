17bf9d245e8748f7baace837255ec09b0a5970ee
szigmon <SOME_RANDOM_ALIAS@example.com>
2023-03-29 01:46:32 +0300

    argocd

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -33,7 +33,6 @@ import { PluginEnvironment } from './types';
 import { ServerPermissionClient } from '@backstage/plugin-permission-node';
 import { DefaultIdentityClient } from '@backstage/plugin-auth-node';
 import ocm from './plugins/ocm';
-import argocd from './plugins/argocd';
 
 function makeCreateEnv(config: Config) {
   const root = getRootLogger();
@@ -90,7 +89,6 @@ async function main() {
   const appEnv = useHotMemoize(module, () => createEnv('app'));
   const kubernetesEnv = useHotMemoize(module, () => createEnv('kubernetes'));
   const ocmEnv = useHotMemoize(module, () => createEnv('ocm'));
-  const argocdEnv = useHotMemoize(module, () => createEnv('argocd'));
 
   const apiRouter = Router();
   apiRouter.use('/catalog', await catalog(catalogEnv));
@@ -101,7 +99,6 @@ async function main() {
   apiRouter.use('/search', await search(searchEnv));
   apiRouter.use('/kubernetes', await kubernetes(kubernetesEnv));
   apiRouter.use('/ocm', await ocm(ocmEnv));
-  apiRouter.use('/argocd', await argocd(argocdEnv));
 
   // Add backends ABOVE this line; this 404 handler is the catch-all fallback
   apiRouter.use(notFoundHandler());
